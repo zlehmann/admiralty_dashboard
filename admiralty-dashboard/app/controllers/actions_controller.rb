40384e22c0059e891e0f268648a9fe709c6ebb8b
zlehmann <zlehmann@gmail.com>
2019-02-15 14:00:58 -0800

    Adds actions nested resources and creates Action index view.

--- admiralty-dashboard/app/controllers/actions_controller.rb
+++ admiralty-dashboard/app/controllers/actions_controller.rb
@@ -1,2 +1,11 @@
 class ActionsController < ApplicationController
+
+    def index
+        if params[:captain_id]
+            @actions = Captain.find(params[:captain_id]).actions
+        else 
+            @actions = Action.all
+        end
+    end
+
 end
