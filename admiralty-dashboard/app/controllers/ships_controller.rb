5ffdb56ae669a1407b119876cec46ca82a57ef24
zlehmann <zlehmann@gmail.com>
2019-02-18 15:24:30 -0800

    adds form to add ships and assign them a captain

--- admiralty-dashboard/app/controllers/ships_controller.rb
+++ admiralty-dashboard/app/controllers/ships_controller.rb
@@ -8,4 +8,26 @@ class ShipsController < ApplicationController
     def show
         @ship = Ship.find(params[:id])
     end
+
+    def new
+        @ship = Ship.new 
+    end
+
+    def create 
+        @ship = Ship.new(ship_params)
+        @ship.captains << Captain.find(params[:ship][:captain_id])
+        binding.pry
+        if @ship.save
+            redirect_to ship_path(@ship.id)
+        else
+            render :new 
+        end
+    end
+
+    private
+
+    def ship_params
+        params.require(:ship).permit(:name, :guns, :class_type, :captain_id)
+    end
+
 end
