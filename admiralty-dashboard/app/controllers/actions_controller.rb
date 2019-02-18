1042a83991c9ec2404e47642e906da50ba0ddb6e
zlehmann <zlehmann@gmail.com>
2019-02-18 11:12:07 -0800

    Adds index and show pages for ships and actions.

--- admiralty-dashboard/app/controllers/actions_controller.rb
+++ admiralty-dashboard/app/controllers/actions_controller.rb
@@ -8,4 +8,12 @@ class ActionsController < ApplicationController
         end
     end
 
+    def show
+        @action = Action.find(params[:id])
+        @ship = Ship.find(@action.ship_id)
+        if params[:captain_id]
+            @captain = Captain.find(params[:captain_id])
+        end
+    end
+
 end
