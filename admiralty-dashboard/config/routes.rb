f6650157e6cc70e3b926cf86262d8185c478e1ce
zlehmann <zlehmann@gmail.com>
2019-02-18 15:09:04 -0800

    Adds links to make new models on index pages.

--- admiralty-dashboard/config/routes.rb
+++ admiralty-dashboard/config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  get '/403', to: 'sessions#forbidden'
+  get '/403', to: 'application#forbidden'
 end
