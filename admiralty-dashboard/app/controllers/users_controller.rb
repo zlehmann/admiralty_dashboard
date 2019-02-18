57d7cb430176f4905c187e1b0d210477edf275af
zlehmann <zlehmann@gmail.com>
2019-02-18 13:11:27 -0800

    Adds create functionality to Users.

--- admiralty-dashboard/app/controllers/users_controller.rb
+++ admiralty-dashboard/app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new 
+    end
 
+    def create 
+        binding.pry
+        @user = User.create(user_params)
+        if @user.save 
+            redirect_to user_path(@user.id)
+        else 
+            render :new
+        end
+    end
 
     def show
         if params[:captain_id]
@@ -7,4 +19,9 @@ class UsersController < ApplicationController
         end 
     end
 
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :age, :nationality, :password, :password_confirmation)
+    end
 end
